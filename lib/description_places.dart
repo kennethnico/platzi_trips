bf2569120861877a14246cb82dff5f21299de622
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-03-15 14:16:43 -0600

    Comenzando antes de que termine jaja

---
+++ lib/description_places.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget{
+  @override
+  Widget build(BuildContext){
+    //TODO: implement build
+    return null;
+  }
+}
