7dcd2ca632f54cb19f09a135c2b5b7731ae5cddc
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-03-15 14:23:51 -0600

    Elemento Texto

--- lib/description_places.dart
+++ lib/description_places.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget{
   @override
-  Widget build(BuildContext){
+  Widget build(BuildContext context){
     //TODO: implement build
+    final title_stars = Row(
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: const Text(
+            'Duwili Ella',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+      ],
+    );
     return null;
   }
 }
