4f57e4d6fc7ce89d98c3957d4083cafa6c4aebc5
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-03-28 13:49:09 -0600

    Gradiente Profile

--- lib/profile_card_description_list.dart
+++ lib/profile_card_description_list.dart
@@ -11,9 +11,9 @@ class ProfileCardDescriptionList extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          ProfileCard("assets/image/wp.png","Salón de Usos Múltiples", "Instituto de Estudios Judiciales del Poder Judicial de la Ciudad de México", "Niños Héroes #150"),
-          ProfileCard("assets/image/wp.png","Salón de Usos Múltiples", "Instituto de Estudios Judiciales del Poder Judicial de la Ciudad de México", "Niños Héroes #150"),
-          ProfileCard("assets/image/wp.png","Salón de Usos Múltiples", "Instituto de Estudios Judiciales del Poder Judicial de la Ciudad de México", "Niños Héroes #150"),
+          ProfileCard("assets/image/wpp3.jpg","Salón de Usos Múltiples", "Instituto de Estudios Judiciales del Poder Judicial de la Ciudad de México", "Niños Héroes #150"),
+          ProfileCard("assets/image/wpp1.jpeg","Auditorio \"Benito Juárez\"", "Poder Judicial de la Ciudad de México", "Niños Héroes #132"),
+          ProfileCard("assets/image/wpp2.jpeg","Salón de Usos Múltiples", "Instituto de Estudios Judiciales del Poder Judicial de la Ciudad de México", "Niños Héroes #150"),
           ProfileCard("assets/image/wp.png","Salón de Usos Múltiples", "Instituto de Estudios Judiciales del Poder Judicial de la Ciudad de México", "Niños Héroes #150"),
         ],
       ),
--- lib/profile_trips.dart
+++ lib/profile_trips.dart
@@ -8,8 +8,8 @@ class ProfileTrips extends StatelessWidget {
       children: <Widget>[
         //ProfileDescriptionImage("Knuckles Mountains Range", "Debug service listening on ws://127.0.0.1:54594/UhAK4hQHRbo=", "Steps: 123,123,123")
         ListView(
-          children: <Widget>[
-            ProfileCardDescriptionList()
+          children: const <Widget>[
+            //ProfileCardDescriptionList()
           ],
         )
       ],
