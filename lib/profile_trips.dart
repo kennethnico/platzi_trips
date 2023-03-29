2599f001e228345e103306a10fa649af6bc09c35
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-03-29 09:45:09 -0600

    Gradiente Profile 100%

--- lib/profile_trips.dart
+++ lib/profile_trips.dart
@@ -7,8 +7,6 @@ class ProfileTrips extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        //ProfileDescriptionImage("Knuckles Mountains Range", "Debug service listening on ws://127.0.0.1:54594/UhAK4hQHRbo=", "Steps: 123,123,123")
-        //ProfileGradientBack("Profile"),
         HeaderProfileAppBar(),
         ListView(
           children: <Widget>[
